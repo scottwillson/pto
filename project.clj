0c6a5633f6ed20fe1aea428e5873c7e86e405242
Scott Willson <s.willson@flyblade.com>
2019-07-23 11:57:30 -0400

    Add web dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,18 @@
 (defproject pto "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [
-    [clojure.java-time "0.3.2"]
-    [org.clojure/clojure "1.10.0"]]
-  :main ^:skip-aot pto.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+            :description "Simple exercise to calculate PTO"
+            :url "http://example.com/FIXME"
+            :license {:name "The MIT License"
+                      :url "http://opensource.org/licenses/MIT"}
+            :dependencies [[clojure.java-time "0.3.2"]
+                           [compojure "1.6.1"]
+                           [hiccup "1.0.5"]
+                           [ring/ring-defaults "0.3.2"]
+                           [org.clojure/clojure "1.10.0"]
+                           [org.clojure/java.jdbc "0.7.9"]]
+            :main ^:skip-aot pto.core
+            :plugins [[lein-ring "0.12.5"]]
+            :ring {:handler pto.handler/app}
+            :target-path "target/%s"
+            :profiles {:uberjar {:aot :all}
+                       :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                            [ring/ring-mock "0.3.2"]]}})
